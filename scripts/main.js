13f88784644967a2292e363b3563becf4735fd36
ramon <mundomon@gmail.com>
2024-11-26 05:02:05 +0100

    No Funcional: Multiples Mapas. Faltan poner elementos en mapa

--- scripts/main.js
+++ scripts/main.js
@@ -2,19 +2,16 @@
 // The number for the player to guess - a random number from 0-100.
 const theNumber = Math.floor(Math.random() * 100);
 
-// Instances of TextInput and ResultText
-let marray = null;
-let imgbt1 = null;
-let imgbt2 = null;
-
-let resultTextInst = null;
-
 //globales
+let imgbtn = null;
 let gameArray = null;
+let mapasArray = null;
 let figurasArray = null;
-let spriteAnimales = null;
 let NUM_BTNS = null;
-let NUM_FIG = 8;
+let NUM_FIG = 0;
+let NUM_MAPS_L1 = 0;
+let NUM_RONDAS = 0;
+let mapas = null;
 
 
 // Add handlers to get "beforeprojectstart" event
@@ -25,59 +22,47 @@ runOnStartup(async runtime =>
 
 async function OnBeforeProjectStart(runtime)
 {
-	// Now the runtime is ready, get the TextInput and ResultText instances
-	//textInputInst = runtime.objects.TextInput.getFirstInstance();
-	resultTextInst = runtime.objects.Iniciar.getFirstInstance();
-	imgbt1 = runtime.objects.ImgBtn.getFirstInstance();
-	//var mav = runtime.globalVars.RandomButtonsJson.getFirstInstance();
-	gameArray = runtime.objects.ArrayAdivina.getFirstInstance(); //elementos a adivinar
-	figurasArray = runtime.objects.ArrayFiguras.getFirstInstance(); //Todos los elementos
-	NUM_BTNS = runtime.globalVars.ap_NUM_BUT;
 	
-	marray = generarArrayUnico();
+	gameArray = runtime.objects.ArrayAdivina.getFirstInstance(); //elementos a adivinar
+	mapasArray = runtime.objects.ArrayMapas.getFirstInstance(); //mapas para la partida
+	NUM_BTNS = runtime.globalVars.ap_NUM_BUT;
+	mapas = runtime.objects.ap_MapaImagen.getFirstInstance();
+	NUM_MAPS_L1 = runtime.globalVars.ap_NUM_MAPS_LEVEL1;
+	NUM_RONDAS  = runtime.globalVars.ap_NUM_RONDAS;
+	
+	
+
 }
 
-
-// Export functions to get the number, the guessed number, and show a message.
-
-export function getEnteredNumber()
-{
-	return Number(textInputInst.text);	// convert text to number
-}
-
-// Pone el nombre de los animales en un array, en el mismo orden que los frames del sprite iap_animales
-// Rellena el GameArray (el object ArraFiguras) con frames aleatorios sin rep
-export function rellenarGameArray()
-{
-	marray = generarArrayUnico();
+// Rellena ArrayFiguras (GameArray) con frames aleatorios sin repeticion
+export function ap_rellenarGameArrayDeBotones(numMapa, newImgbtn)
+{	imgbtn = newImgbtn;
+	let marray = generarArrayBotonesUnico(numMapa);
 	for(var i=0;i<marray.length;i++){
 		gameArray.setAt(marray[i],i,0,0);
 	}
-	//resultTextInst.text = JSON.stringify(marray);//marray.join(', ');//message;
-	
-	//Relleno el array de figuras con el nombre de los animales (por orden en delos frames)
-	figurasArray.setAt("Mapache",0,0,0);
-	figurasArray.setAt("Pajaro",1,0,0);
-	figurasArray.setAt("Pez",2,0,0);
-	figurasArray.setAt("Erizo",3,0,0);
-	figurasArray.setAt("Lagartija",4,0,0);
-	figurasArray.setAt("Ardilla",5,0,0);
-	figurasArray.setAt("Rana",6,0,0);
-	figurasArray.setAt("Tortuga",7,0,0);
-	
 }
-
-export function getTheArray(i)
+export function ap_rellenarGameArrayDeMapas()
 {
-	return marray[i];
+	let marray = generarArrayMapasUnico();
+	for(var i=0;i<marray.length;i++){
+		mapasArray.setAt(marray[i],i,0,0);
+	}
+ 
 }
 
-
-
-//Genera un array de numeros (de frames) sin repeticion de elementos con el numero de imagenes (frames) del sprite
-function generarArrayUnico() {
-	
+/*Genera un array de numeros (de frames) sin repeticion de elementos con el numero de imagenes (frames) del sprite. 
+Recibe numMapa, el mapa (animacion) de MapaImagen que se esta utilizando
+*/
+function generarArrayBotonesUnico(numMapa) {
   var array = [];
+  
+  imgbtn.setAnimation("Animation " + numMapa);
+  
+  let NUM_FIG = imgbtn.animation.frameCount;
+  
+  console.log("Animation:"+numMapa+"- numFrames:" + NUM_FIG);
+  
   // Generar NUM_BTNS números aleatorios únicos.
   while (array.length < NUM_BTNS) {
     var numeroAleatorio = Math.floor(Math.random() * NUM_FIG); // Genera un número aleatorio del 0 al Numero de figuras que haya
@@ -85,8 +70,24 @@ function generarArrayUnico() {
       array.push(numeroAleatorio); // Agrega el número al array
     }
   }
-  
-
   return array;
 }
 
+//Genera un array con los mapas a jugar en cada ronda
+function generarArrayMapasUnico(){ 
+  //imgbt1.setAnimation("Animation " + numAnim);
+  
+  //console.log("numFrames:" + imgbtn.animation.frameCount);
+  
+  var array = [];
+  // Generar NUM_RONDAS numeros aleatorios únicos.
+  console.log("rondas: "+NUM_RONDAS+" - Maps: "+NUM_MAPS_L1);
+  while (array.length < NUM_RONDAS) {
+    var numeroAleatorio = Math.floor(Math.random() * NUM_MAPS_L1); // Genera un número aleatorio del 0 al Numero de mapas que tenga el nivel
+    if (array.indexOf(numeroAleatorio) === -1) { // Verifica si el número no está en el array
+      array.push(numeroAleatorio); // Agrega el número al array
+    }
+  }
+  return array;
+
+}
