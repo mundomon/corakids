bf5d144ef3d0b04582a93ad30c0e906f04df7484
ramon <mundomon@gmail.com>
2024-12-12 01:21:01 +0100

    Añadido en Corakids

--- scripts/importsForEvents.js
+++ scripts/importsForEvents.js
@@ -1,4 +1,4 @@
 
 // This makes the getTheNumber, getEnteredNumber and showResultMessage
 // functions available in scripts in event sheets.
-import { ap_rellenarGameArrayDeBotones, ap_rellenarGameArrayDeMapas } from "./main.js";
+import { ap_rellenarGameArrayDeBotones, ap_rellenarGameArrayDeMapas } from "./ap_main.js";
